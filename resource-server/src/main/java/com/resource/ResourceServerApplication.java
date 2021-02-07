461d2a04198865f7bc7ed24dbaf05d7049a0136d
Tinhnx <nhoccon2111@gmail.com>
2021-02-07 14:04:13 +0700

    init

---
+++ resource-server/src/main/java/com/resource/ResourceServerApplication.java
@@ -0,0 +1,18 @@
+package com.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
+
+import com.common.config.CorsFilter;
+
+@SpringBootApplication
+@Import(value = { CorsFilter.class })
+public class ResourceServerApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceServerApplication.class, args);
+	}
+
+}
